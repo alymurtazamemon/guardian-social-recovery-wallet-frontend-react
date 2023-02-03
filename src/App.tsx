d718faa01739fe405a9a7a152966d8e77efe162a
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-02-03 20:07:09 +0500

    create Header component and update website background

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-    return <div>Hello World</div>;
+    return (
+        <div className="mx-72">
+            <Header />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header(): JSX.Element {
+    return (
+        <div className="flex items-center">
+            <h1 className="text-4xl mr-4">👼</h1>
+            <h1 className="tracking-widest my-8 text-xl pt-1">GUARDIANS</h1>
+        </div>
+    );
+}
+
+export default Header;

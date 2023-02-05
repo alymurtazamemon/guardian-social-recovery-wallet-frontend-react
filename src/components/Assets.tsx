590eb487639e26b4801463335e462a522e9221df
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-02-05 13:58:43 +0500

    handle amount deposit - Assets

--- src/components/Assets.tsx
+++ src/components/Assets.tsx
@@ -3,19 +3,26 @@ import {
     BsFillArrowDownCircleFill,
     BsFillArrowUpRightCircleFill,
 } from "react-icons/bs";
-import { useMoralis, useWeb3Contract } from "react-moralis";
+import { useWeb3Contract } from "react-moralis";
 import { useEffect, useState } from "react";
-import { ethers } from "ethers";
 import { abi } from "../constants";
-import { Input } from "@web3uikit/core";
+import { Input, useNotification } from "@web3uikit/core";
+import { AiFillBell } from "react-icons/ai";
+import { ContractTransaction, ethers } from "ethers";
 
 interface AssetsPropsTypes {
     guardianContractAddress: string;
 }
 
+const { ethereum } = window as any;
+
 function Assets({ guardianContractAddress }: AssetsPropsTypes): JSX.Element {
+    const dispatch = useNotification();
+
     const [walletBalance, setWalletBalance] = useState("0");
     const [walletBalanceInUSD, setWalletBalanceInUSD] = useState("0");
+    const [amount, setAmount] = useState<number>();
+    const [address, setAddress] = useState("");
 
     const { runContractFunction: getBalance } = useWeb3Contract({
         abi: abi,
@@ -41,23 +48,57 @@ function Assets({ guardianContractAddress }: AssetsPropsTypes): JSX.Element {
         })();
     }, []);
 
+    async function handleOnDepositClick() {
+        if (amount == undefined) {
+            dispatch({
+                type: "warning",
+                title: "Amount Not Found",
+                message: "Please enter deposit amount in the amount field.",
+                icon: <AiFillBell />,
+                position: "topR",
+            });
+            return;
+        }
+
+        if (typeof ethereum != "undefined") {
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+
+            const tx: ContractTransaction = await signer.sendTransaction({
+                to: guardianContractAddress,
+                data: "0x",
+                value: ethers.utils.parseEther(amount.toString()),
+            });
+
+            await tx.wait(1);
+
+            dispatch({
+                type: "success",
+                title: "Successs",
+                message: `${amount} successfully deposited.`,
+                icon: <AiFillBell />,
+                position: "topR",
+            });
+
+            setAmount(undefined);
+        }
+    }
+
     return (
         <div className="flex flex-col justify-czenter items-center mt-8">
             <div className="border border-gray-400 w-fit p-2 rounded-full">
                 <SiEthereum color="black" size={22} />
             </div>
             <h1 className="text-4xl font-medium text-black mt-6">
-                {ethers.formatEther(walletBalance)} ETH
+                {ethers.utils.formatEther(walletBalance)} ETH
             </h1>
             <h2 className="mt-2 mb-4 text-lg">
-                ${ethers.formatEther(walletBalanceInUSD)} USD
+                ${ethers.utils.formatEther(walletBalanceInUSD)} USD
             </h2>
             <div className="flex mt-2">
                 <button
                     className="flex flex-col items-center justify-center mx-4"
-                    onClick={() => {
-                        console.log("Deposit Clicked!");
-                    }}
+                    onClick={handleOnDepositClick}
                 >
                     <BsFillArrowDownCircleFill color="#0D72C4" size={40} />
                     <h1 className="text-[#0D72C4] m-2">Deposit</h1>
@@ -92,6 +133,8 @@ function Assets({ guardianContractAddress }: AssetsPropsTypes): JSX.Element {
                 style={{
                     marginTop: "32px",
                 }}
+                value={amount}
+                onChange={(event) => setAmount(Number(event.target.value))}
             />
             <Input
                 label="Address"
@@ -105,6 +148,8 @@ function Assets({ guardianContractAddress }: AssetsPropsTypes): JSX.Element {
                 style={{
                     marginTop: "32px",
                 }}
+                value={address}
+                onChange={(event) => setAddress(event.target.value)}
             />
         </div>
     );

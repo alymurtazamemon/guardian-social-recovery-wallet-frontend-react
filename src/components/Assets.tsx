311aa447fed75eea80303d0bbb8f13cd990d146b
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-02-06 17:31:46 +0500

    get balance and update in handleSendOnSuccess - Assets

--- src/components/Assets.tsx
+++ src/components/Assets.tsx
@@ -146,12 +146,17 @@ function Assets({ guardianContractAddress }: AssetsPropsTypes): JSX.Element {
     async function handleSendOnSuccess(tx: ContractTransaction) {
         await tx.wait(1);
 
+        const balance = (await getBalance()) as String;
+        const balanceInUSD = (await getBalanceInUSD()) as String;
+
         _showNotification(
             NotificationType.success,
             "Successs",
             `${amount} ETH sent to ${address}`
         );
 
+        setWalletBalance(balance.toString());
+        setWalletBalanceInUSD(balanceInUSD.toString());
         setAmount(undefined);
         setAddress("");
     }
